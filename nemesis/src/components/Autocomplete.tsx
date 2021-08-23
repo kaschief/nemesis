4c777e34ca566c30b8d6242e27afc64bf6e019ae
Kaschief Johnson <kaschiefj@gmail.com>
2021-08-23 16:24:43 +0100

    main feature properties complete

--- nemesis/src/components/Autocomplete.tsx
+++ nemesis/src/components/Autocomplete.tsx
@@ -4,12 +4,63 @@ import { useIsMount } from "../useIsMount";
 
 export const AutoComplete: React.FC = () => {
   const [input, setInput] = useState("");
-  const [isActive, setIsActive] = useState(false);
+  const [displayedText, setDisplayedText] = useState("");
+  const [showSuggestions, setShowSuggestions] = useState(false);
   const [filteredOptions, setFilteredOptions] = useState([""]);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setInput(e.target.value.toLowerCase());
-    setIsActive(true);
+    setDisplayedText(e.target.value);
+
+    setShowSuggestions(true);
+  };
+
+  const hoverChange = (e: React.MouseEvent<HTMLDivElement>): void => {
+    const element = e.target as HTMLElement;
+    const elementText: string = element.children[0].innerHTML;
+    const activeIndex: unknown = element.getAttribute("data-id");
+    console.log(activeIndex);
+
+    setDisplayedText(elementText);
+    setSelectedIndex(activeIndex as number);
+  };
+
+  const handleClick = (e: React.MouseEvent<HTMLDivElement>): void => {
+    const element = e.target as HTMLElement;
+    const elementText: string = element.children[0].innerHTML;
+
+    setInput(elementText);
+    setDisplayedText(elementText);
+    setFilteredOptions([]);
+    setTimeout(() => {
+      setShowSuggestions(false);
+    }, 300);
+  };
+
+  const handleOptionSelect = (
+    e: React.KeyboardEvent<HTMLInputElement>
+  ): JSX.Element | undefined => {
+    if (e.code === "Enter") {
+      setInput(filteredOptions[selectedIndex]);
+      setSelectedIndex(0);
+      setTimeout(() => {
+        setShowSuggestions(false);
+      }, 300);
+    } else if (e.code === "ArrowUp") {
+      if (selectedIndex === 0) {
+        return;
+      }
+      setSelectedIndex(selectedIndex - 1);
+      setDisplayedText(filteredOptions[selectedIndex - 1]);
+    } else if (e.code === "ArrowDown") {
+      if (selectedIndex - 1 === filteredOptions.length) {
+        return;
+      } else if (selectedIndex + 1 < filteredOptions.length) {
+        setSelectedIndex(selectedIndex + 1);
+        setDisplayedText(filteredOptions[selectedIndex + 1]);
+      }
+    }
   };
 
   const isMount: boolean = useIsMount();
@@ -24,18 +75,30 @@ export const AutoComplete: React.FC = () => {
   }, [isMount, input]);
 
   return (
-    <div>
+    <div className="autocomplete">
       <input
         name="search"
         placeholder="search"
         type="text"
-        value={input}
+        value={displayedText}
         onChange={handleChange}
+        onKeyDown={handleOptionSelect}
       />
 
-      {filteredOptions.map((fruit: string, index: number) => {
-        return <div key={index}>{fruit}</div>;
-      })}
+      {showSuggestions &&
+        filteredOptions.map((fruit: string, index: number) => {
+          return (
+            <div
+              className={`list-item ${index === selectedIndex && "selected"}`}
+              key={index}
+              onMouseEnter={hoverChange}
+              onClick={handleClick}
+              data-id={index}
+            >
+              <p>{fruit}</p>
+            </div>
+          );
+        })}
     </div>
   );
 };
